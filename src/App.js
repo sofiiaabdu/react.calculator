64f2a4f5a8c0eb04cdeacb64e5b5b8370b589d8f
sofiiaabdu <sofiiaa@tahometer.com>
2018-09-13 10:28:13 +0300

    #3 Added the class Board.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,56 @@ function Key(props) {
     );
 }
 
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+            operator: null,
+        };
+    }
 
+    inputNumber(number) {
+
+    }
+
+    render() {
+        return (
+            <div className="Game">
+                <Board data={this.state.operations} />
+                <Keys>
+                    <Key onClick={this.handleClick} label="C" value="clear" />
+                    <Key onClick={this.handleClick} label="7" value="7" />
+                    <Key onClick={this.handleClick} label="4" value="4" />
+                    <Key onClick={this.handleClick} label="1" value="1" />
+                    <Key onClick={this.handleClick} label="0" value="0" />
+
+                    <Key onClick={this.handleClick} label="/" value="/" />
+                    <Key onClick={this.handleClick} label="8" value="8" />
+                    <Key onClick={this.handleClick} label="5" value="5" />
+                    <Key onClick={this.handleClick} label="2" value="2" />
+                    <Key onClick={this.handleClick} label="." value="." />
+
+                    <Key onClick={this.handleClick} label="x" value="*" />
+                    <Key onClick={this.handleClick} label="9" value="9" />
+                    <Key onClick={this.handleClick} label="6" value="6" />
+                    <Key onClick={this.handleClick} label="3" value="3" />
+                    <Key label="" value="null" />
+
+                    <Key onClick={this.handleClick} label="-" value="-" />
+                    <Key onClick={this.handleClick} label="+" size="2" value="+" />
+                    <Key onClick={this.handleClick} label="=" size="2" value="equal" />
+                </Keys>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    constructor() {
+        super()
+        this.state = { operations: [] }
+    }
+}
 
 export default Game;
