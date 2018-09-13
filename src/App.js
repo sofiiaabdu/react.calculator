9891435ba2b3adabac876db10f3f9f4f9ddd72f9
sofiiaabdu <sofiiaa@tahometer.com>
2018-09-13 10:27:31 +0300

    Added the class Key for buttons.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+function Key(props) {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <button className="key"
+                onClick={props.onClick}
+                data-value={props.value}>
+            {props.label}
+        </button>
     );
-  }
 }
 
-export default App;
+
+
+export default Game;
