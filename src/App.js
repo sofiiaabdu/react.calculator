f3dfa3596ca5cad14d08ee4c8faaaece3de43819
sofiiaabdu <sofiiaa@tahometer.com>
2018-09-14 15:36:16 +0300

    Fixed validation.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,42 @@
 import React, { Component } from 'react';
-import update from 'immutability-helper';
 import Display from './Display';
+import Key from './Key';
 import './App.css';
-
-function Key(props) {
-    return (
-        <button className="key"
-                onClick={props.onClick}
-                data={props.value}>
-            {props.label}
-        </button>
-    );
-}
 
 class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            keys: "",
+            numbers: "",
         };
     }
 
     calculateResult = () => {
-        let result = this.state.keys;
+        let result = this.state.numbers;
 
         if (result) {
-            result = eval(result);
+            try {
+                result = eval(result);
+            } catch (e) {
+                result = "Invalid Expression";
+            }
             this.setState({
-                keys: result
+                numbers: result
             });
         }
     };
 
     handleClick(digit) {
-        let temp = this.state.keys;
+        let temp = this.state.numbers;
         switch (digit) {
             case "CE":
                 if (temp.length > 0) {
                     temp = temp.slice(0, -1);
                 }
-                this.setState({keys: temp});
+                this.setState({numbers: temp});
                 break;
             case "C":
-                this.setState({keys: ""});
+                this.setState({numbers: ""});
                 break;
             case "equal":
                 this.calculateResult();
@@ -53,49 +47,48 @@ class Board extends React.Component {
                 });*/
                 temp = temp + digit.toString();
                 this.setState({
-                    keys: temp,
+                    numbers: temp,
                 });
                 break;
         }
     }
 
-    renderKey(digit, label) {
+    renderKey(digit, label, type="single") {
         return (
-            <Key onClick={() => this.handleClick(digit)} value={digit} label={label}/> )
+            <Key onClick={() => this.handleClick(digit)} value={digit} label={label} type={type}/> )
     }
 
     render() {
         return (
             <div className="board">
-                <Display data={this.state.keys} />
+                <Display data={this.state.numbers} />
+                <div className="board-row">
+                    {this.renderKey("C", "C", "clear")}
+                    {this.renderKey("CE", "CE", "double clear")}
+                    {this.renderKey("/", "/", "operation")}
+                </div>
                 <div className="board-row">
                     {this.renderKey(7, "7")}
-                    {this.renderKey(4, "4")}
-                    {this.renderKey(1, "1")}
-                    {this.renderKey(".", ".")}
-                    {this.renderKey("C", "C")}
-                </div>
-                <div className="board-row">
                     {this.renderKey(8, "8")}
-                    {this.renderKey(5, "5")}
-                    {this.renderKey(2, "2")}
-                    {this.renderKey("*", "x")}
-                    {this.renderKey("/", "/")}
-                </div>
-                <div className="board-row">
                     {this.renderKey(9, "9")}
-                    {this.renderKey(6, "6")}
-                    {this.renderKey(3, "3")}
-                    {this.renderKey("+", "+")}
-                    {this.renderKey("-", "-")}
-
+                    {this.renderKey("*", "x","operation")}
                 </div>
                 <div className="board-row">
-                    {this.renderKey(null)}
+                    {this.renderKey(4, "4")}
+                    {this.renderKey(5, "5")}
+                    {this.renderKey(6, "6")}
+                    {this.renderKey("-", "-", "operation")}
+                </div>
+                <div className="board-row">
+                    {this.renderKey(3, "3")}
+                    {this.renderKey(2, "2")}
+                    {this.renderKey(1, "1")}
+                    {this.renderKey("+", "+", "operation")}
+                </div>
+                <div className="board-row">
                     {this.renderKey(0, "0")}
-                    {this.renderKey(null)}
-                    {this.renderKey("CE", "CE")}
-                    {this.renderKey("equal", "=")}
+                    {this.renderKey(".", ".")}
+                    {this.renderKey("equal", "=", "double operation")}
                 </div>
             </div>
         );
